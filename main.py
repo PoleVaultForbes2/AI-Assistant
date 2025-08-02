5e182fb693da6356488f0a2e029ec0e6994ce2ce
PoleVaultForbes2 <167945665+PoleVaultForbes2@users.noreply.github.com>
2025-08-02 11:47:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 # main.py
-# from langchain.agents import initialize_agent
-# from langchain.agents.agent_types import AgentType
-# from ollama_model import get_ollama_model
-# from tools import all_tools
-
 from langgraph_agent import graph
 
 
@@ -19,23 +14,6 @@ def main():
 
         print("AI:", result.get("output", "No response"))
 
-    # FOR LANGCHAIN
-    # llm = get_ollama_model()
-    # agent = initialize_agent(
-    #     tools=all_tools,
-    #     llm=llm,
-    #     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    #     verbose=True
-    # )
-    #
-    # print("Type something to your assistant (or 'exit'):")
-    # while True:
-    #     user_input = input("You: ")
-    #     if user_input.lower() in ["exit", "quit"]:
-    #         break
-    #     response = agent.run(user_input)
-    #     print("AI:", response)
-
-
 if __name__ == "__main__":
+
     main()
